a1cbe92564c61ff1667161a050fad5d28a08f6b2
adimensional <arthursbdantas@gmail.com>
2026-02-18 19:09:20 -0300

    new UI, dark theme, working progress

---
+++ app/src/main/java/com/pomodororo/PomodoroModel.kt
@@ -0,0 +1,13 @@
+// model/PomodoroModel.kt
+package com.pomodororo.model
+
+data class PomodoroModel(
+    val tag: String = "study",
+    val focusSeconds: Int = 25 * 60,
+    val restSeconds: Int = 5 * 60,
+    val currentPhase: String = "focus",
+    val remainingSeconds: Int = 25 * 60,
+    val completedSessions: Int = 0,
+    val totalSessions: Int = 4,
+    val isRunning: Boolean = true
+)
